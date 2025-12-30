90b0e823f708754f232d64b7f7ce71d644ce6bba
Sonic7002 <srijankargupta470@gmail.com>
2025-12-30 22:43:29 +0530

    optimized model file

--- model.py
+++ model.py
@@ -1,15 +1,15 @@
 # this file contains template for all tasks and a date time function
 from datetime import datetime
 
+def timelog() -> str:
+    return str(datetime.now().strftime('%H:%M:%S %d-%m-%Y'))
+
 def task_model(identity: int, msg: str) -> dict:
     new = {
             "ID": identity,
             "description": msg,
             "status": "todo",
-            "created": str(datetime.now().strftime('%H:%M:%S %d-%m-%Y')),
-            "updated": str(datetime.now().strftime('%H:%M:%S %d-%m-%Y'))
+            "created": timelog(),
+            "updated": timelog()
         }
-    return new
-
-def timelog() -> str:
-    return str(datetime.now().strftime('%H:%M:%S %d-%m-%Y'))
+    return new 
